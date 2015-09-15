e41ecc2919a3aa7d2998acc4631c565b58758115
Gaurav Shukla <gshukla66@gmail.com>
2015-09-15 17:40:25 +0530

    Added Yoeman angular scaffolding. See yoeman website for tutorials

---
+++ app/scripts/app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name probaseUiApp
+ * @description
+ * # probaseUiApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('probaseUiApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name probaseUiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the probaseUiApp
+ */
+angular.module('probaseUiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
